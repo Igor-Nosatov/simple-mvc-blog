623ebcb3b4e3ca330aaaab83763517fe7ca2dfe9
mcoudurier <mathieu.coudurier@gmail.com>
2018-02-22 10:34:50 +0100

    Add route to single post

--- index.php
+++ index.php
@@ -11,6 +11,13 @@ try
         {
             $controller->listPosts();
         }
+        else if($_GET['action'] == 'post')
+        {
+            if (isset($_GET['id']) && $_GET['id'] > 0)
+            {
+                $controller->post();
+            }
+        }
     }
     else
     {
