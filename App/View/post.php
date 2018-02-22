a4b6336fd24ed42c1bffd89ba757b6cfa3e66433
mcoudurier <mathieu.coudurier@gmail.com>
2018-02-22 10:40:02 +0100

    single post view

---
+++ App/View/post.php
@@ -0,0 +1,8 @@
+<article class="post--single">
+    <header class="post-header">
+        <h3 class="post-heading">
+            <?= $post->getTitle() ?>
+        </h3>
+    </header>
+    <?= $post->getContent() ?>
+</article>
