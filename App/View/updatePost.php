f7826831f5cfb03533b330be57b4341f05186edc
mcoudurier <mathieu.coudurier@gmail.com>
2018-02-24 21:08:56 +0100

    Add updatePost view

---
+++ App/View/updatePost.php
@@ -0,0 +1,18 @@
+<?php 
+$title = 'Modifier le post';
+ob_start(); 
+?>
+
+<form action="index.php?action=executeUpdatePost&id=<?= $post->getId() ?>" method="post">
+    <input type="text" name="title" id="title" value="<?= $post->getTitle() ?>">
+    <textarea name="content" id="content" cols="30" rows="10"><?= $post->getContent() ?></textarea>
+    <input type="submit" value="Valider">
+</form>
+
+<script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+<script>tinymce.init({ selector:'textarea' });</script>
+
+<?php 
+$content = ob_get_clean();
+require('template.php');
+?>
