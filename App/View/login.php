dd49d5d9d3bc48c40792f898061dfe84fb7d7ebc
mcoudurier <mathieu.coudurier@gmail.com>
2018-03-01 06:10:28 +0100

    WIP html rework with bootstrap integration

--- App/View/login.php
+++ App/View/login.php
@@ -4,9 +4,15 @@ ob_start();
 ?>
 
 <form action="index.php?action=authenticate" method="post" class="login-form">
-    <input type="text" name="username" id="username">
-    <input type="password" name="password" id="password">
-    <input type="submit" value="Login">
+    <div class="form-group">
+        <label for="username">Nom d'utilisateur</label>
+        <input type="text" name="username" id="username" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="password">Mot de passe</label>
+        <input type="password" name="password" id="password" class="form-control">
+    </div>
+    <input type="submit" value="Login" class="btn btn-primary">
 </form>
 
 <?php 
--- App/View/post.php
+++ App/View/post.php
@@ -12,27 +12,37 @@ ob_start();
     <?= $post->getContent() ?>
 </article>
 
-<?php
-foreach ($comments as $comment):
-?>
-    <article class="comment">
-        <header class="comment-header">
-            <h4 class="comment-heading">
-                <?= htmlspecialchars($comment->getAuthor()) ?> 
-            </h4>
-        </header>
-        <?= nl2br(htmlspecialchars($comment->getContent())) ?>
-        <a href="index.php?action=flagComment&id=<?= $comment->getId() ?>">Signaler</a>
-    </article>
-<?php
-endforeach;
-?>
+<section>
+    <h4>Commentaires</h4>
+    <?php
+    foreach ($comments as $comment):
+    ?>
+        <article class="comment">
+            <header class="comment-header">
+                <h4 class="comment-heading">
+                    <?= htmlspecialchars($comment->getAuthor()) ?> 
+                </h4>
+            </header>
+            <?= nl2br(htmlspecialchars($comment->getContent())) ?>
+            <a href="index.php?action=flagComment&id=<?= $comment->getId() ?>">Signaler</a>
+        </article>
+    <?php
+    endforeach;
+    ?>
 
-<form action="index.php?action=addComment&id=<?= $post->getId() ?>" method="post" class="comment-form">
-    <input type="text" name="author" id="author">
-    <textarea name="content" id="content" cols="30" rows="10"></textarea>
-    <input type="submit" value="Envoyer">
-</form>
+    <h4>Ajouter un commentaire</h4>
+    <form action="index.php?action=addComment&id=<?= $post->getId() ?>" method="post" class="comment-form">
+        <div class="form-group">
+            <label for="author">Nom</label>
+            <input type="text" name="author" id="author" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="content">Commentaire</label>
+            <textarea name="content" id="content" cols="30" rows="10" class="form-control"></textarea>
+        </div>
+        <input type="submit" value="Envoyer" class="btn btn-primary">
+    </form>
+</section>
 
 <?php 
 $content = ob_get_clean();
