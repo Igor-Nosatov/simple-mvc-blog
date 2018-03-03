45e07afb6ad2a5d5497e6c40323be04cf5d04d5c
mcoudurier <mathieu.coudurier@gmail.com>
2018-03-03 11:13:08 +0100

    Finish adding the new routes

--- index.php
+++ index.php
@@ -58,6 +58,97 @@ $router->addRoute(new Route(
     ]
 ));
 
+$router->addRoute(new Route(
+    [
+        'url' => '/authenticate',
+        'action' => 'authenticate',
+        'middleware' => '',
+        'vars' => []
+    ]
+));
+
+$router->addRoute(new Route(
+    [
+        'url' => '/addPost',
+        'action' => 'addPost',
+        'middleware' => 'Authenticate',
+        'vars' => []
+    ]
+));
+
+$router->addRoute(new Route(
+    [
+        'url' => '/updatePost/[0-9]+',
+        'action' => 'updatePost',
+        'middleware' => 'Authenticate',
+        'vars' => [
+            'id'
+        ]
+    ]
+));
+
+$router->addRoute(new Route(
+    [
+        'url' => '/executeUpdatePost',
+        'action' => 'executeUpdatePost',
+        'middleware' => 'Authenticate',
+        'vars' => []
+    ]
+));
+
+$router->addRoute(new Route(
+    [
+        'url' => '/deletePost/[0-9]+',
+        'action' => 'deletePost',
+        'middleware' => 'Authenticate',
+        'vars' => [
+            'id'
+        ]
+    ]
+));
+
+$router->addRoute(new Route(
+    [
+        'url' => '/flagComment/[0-9]+',
+        'action' => 'flagComment',
+        'middleware' => '',
+        'vars' => [
+            'id'
+        ]
+    ]
+));
+
+$router->addRoute(new Route(
+    [
+        'url' => '/deleteComment/[0-9]+',
+        'action' => 'deleteComment',
+        'middleware' => 'Authenticate',
+        'vars' => [
+            'id'
+        ]
+    ]
+));
+
+$router->addRoute(new Route(
+    [
+        'url' => '/unflagComment/[0-9]+',
+        'action' => 'unflagComment',
+        'middleware' => 'Authenticate',
+        'vars' => [
+            'id'
+        ]
+    ]
+));
+
+$router->addRoute(new Route(
+    [
+        'url' => '/writePost',
+        'action' => 'writePost',
+        'middleware' => 'Authenticate',
+        'vars' => []
+    ]
+));
+
 try {
     $route = $router->getRoute($_SERVER['REQUEST_URI']);
 }
