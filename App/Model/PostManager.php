b35c1f7aaa8a9417cd0c80ff67e1052c6a5333b9
mcoudurier <mathieu.coudurier@gmail.com>
2018-03-07 06:38:55 +0100

    show the latest posts first

--- App/Model/PostManager.php
+++ App/Model/PostManager.php
@@ -5,7 +5,7 @@ class PostManager extends Manager
 {
     public function getPosts($limit = -1, $offset = -1)
     {
-        $sql = 'SELECT id, title, content, DATE_FORMAT(dateAdded, \'%d/%m/%Y\') AS dateAdded, dateModified FROM posts';
+        $sql = 'SELECT id, title, content, DATE_FORMAT(dateAdded, \'%d/%m/%Y\') AS dateAdded, dateModified FROM posts ORDER BY dateAdded DESC';
 
         if ($limit != -1 || $offset != -1)
         {
