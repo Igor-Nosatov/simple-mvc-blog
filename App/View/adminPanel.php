01e5134c66b715fe0ab6757312e13b68b573f8f2
mcoudurier <mathieu.coudurier@gmail.com>
2018-03-08 09:42:56 +0100

    admin Panel rework

--- App/View/adminPanel.php
+++ App/View/adminPanel.php
@@ -5,27 +5,53 @@ ob_start();
 
 <h1 class="mb-4">Administration</h1>
 
-<a href="/writePost">Ajouter un post</a>
+<div class="row mb-4">
+    <a href="/writePost">Ajouter un post</a>
+</div>
 
-<section>
-    <h3>Commentaires signalés</h4>
-    <?php
-    foreach ($flaggedComments as $flaggedComment):
-    ?>
-        <article>
-            <header>
-                <h4><?= htmlspecialchars($flaggedComment->getAuthor()) ?></h4>
-            </header>
-            <p><?= $flaggedComment->getContent() ?></p>
-            <footer>
-                <a href="/unflagComment/<?= $flaggedComment->getId() ?>">Ignorer</a>
-                <a href="/deleteComment/<?= $flaggedComment->getId() ?>">Supprimer</a>
-            </footer>
-        </article>
-    <?php
-    endforeach;
-    ?>
-</section>
+<div class="row">
+    <table class="table">
+        <caption>Tous les posts</caption>
+        <thead>
+            <tr>
+                <th>Titre</th>
+                <th>Date</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($posts as $post): ?>
+                <tr>
+                    <td>
+                        <a href="/post/<?= $post->getId() ?>"><?= $post->getTitle() ?></a>
+                    </td>
+                    <td>
+                        <?= $post->getDateAdded() ?>
+                    </td>
+                    <td>
+                        <a href="/updatePost/<?= $post->getId() ?>" class="mr-2">Modifier</a>
+                        <a href="/deletePost/<?= $post->getId() ?>">Supprimer</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+
+<h3>Commentaires signalés</h4>
+
+<?php foreach ($flaggedComments as $flaggedComment): ?>
+    <article>
+        <header>
+            <h5><?= htmlspecialchars($flaggedComment->getAuthor()) ?></h5>
+        </header>
+        <p><?= $flaggedComment->getContent() ?></p>
+        <footer>
+            <a href="/unflagComment/<?= $flaggedComment->getId() ?>">Ignorer</a>
+            <a href="/deleteComment/<?= $flaggedComment->getId() ?>">Supprimer</a>
+        </footer>
+    </article>
+<?php endforeach; ?>
 
 <?php 
 $content = ob_get_clean();
