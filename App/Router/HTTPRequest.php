f87fb668846254b0457c39c32fe699bfbd5320a1
mcoudurier <mathieu.coudurier@gmail.com>
2018-03-15 10:18:28 +0100

    add Ajax detection method in HTTPRequest

--- App/Router/HTTPRequest.php
+++ App/Router/HTTPRequest.php
@@ -17,4 +17,9 @@ class HTTPRequest
     {
         return isset($_GET[$key]) ? $_GET[$key] : null;
     }
+
+    public function isAjax() : bool
+    {
+        return !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
+    }
 }
