2b05af0bcf720f47852c6b033d328ddd77e6d72e
mcoudurier <mathieu.coudurier@gmail.com>
2018-03-19 08:35:09 +0100

    Make the side menu a dropdown menu on mobile

--- App/View/listPosts.php
+++ App/View/listPosts.php
@@ -1,16 +1,22 @@
 <?php $title = 'Billet simple pour l\'Alaska'; ?>
 
 <div class="row">
-    <div class="col-md-3 mb-4">
-        <aside class="bg-white border pb-2">
-            <h5 class="text-center p-3 bg-light">Tous les chapitres</h5>
-            <ul class="nav flex-column">
-            <?php foreach ($postsAsc as $postAsc): ?>
-                <li class="nav-item">
-                    <a href="/post/<?= $postAsc->getId() ?>" class="nav-link" ><?= $postAsc->getTitle() ?></a> 
-                </li>
-            <?php endforeach; ?>
-            </ul>
+    <div class="col-md-3 mb-4 mb-md-0">
+        <aside class="bg-white border side-menu">
+            <header class="side-menu__header">
+                <h5 class="text-center p-3 bg-light side-menu__heading m-0">
+                Tous les chapitres<i class="fas fa-caret-down ml-3 d-initial d-md-none caret"></i>
+                </h5>
+            </header>
+            <div class="side-menu__content">
+                <ul class="nav flex-column">
+                <?php foreach ($postsAsc as $postAsc): ?>
+                    <li class="nav-item">
+                        <a href="/post/<?= $postAsc->getId() ?>" class="nav-link" ><?= $postAsc->getTitle() ?></a> 
+                    </li>
+                <?php endforeach; ?>
+                </ul>
+            </div>
         </aside>
     </div>
 
@@ -43,3 +49,5 @@
         </nav>
     </div>
 </div>
+
+<script src="/js/dropdown.js"></script>
